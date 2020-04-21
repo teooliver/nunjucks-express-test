a13f3c25e992bc0ef01e29cfcc5b742d483d1f79
Teo Oliver <teo.liz.oli@gmail.com>
2020-04-21 10:24:21 +0100

    fixed buttons

--- server.js
+++ server.js
@@ -43,6 +43,39 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/room", function (req, res) {
+  res.render("room-created.njk", {
+    orderNumber: "#QG6000000024",
+    date: "18 Apr 2020, 15:00:00",
+    subtotal: "43234",
+    total: "534532",
+    cc_type: "visa",
+    cc_number: "xxxx-0004",
+    shipingMethod: "DHL Express Shipping Rates - EXPRESS WORLDWIDE EU ",
+    rooms: [
+      {
+        label: "Bathroom",
+        property: "Cottage",
+        type: "Modern",
+        img: "https://source.unsplash.com/FBXuXp57eM0",
+        wall1: { colorName: "special blue", color: "#6699ff" },
+        wall2: { colorName: "special orange", color: "orange" },
+        price: 1000,
+      },
+      {
+        label: "Bathroom",
+        property: "Cottage",
+        type: "Modern",
+        img: "https://source.unsplash.com/FBXuXp57eM0",
+        wall1: { colorName: "special purple", color: "purple" },
+        wall2: { colorName: "special yellow", color: "yellow" },
+        ceiling: { colorName: "special yellow", color: "yellow" },
+        price: 1000,
+      },
+    ],
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
